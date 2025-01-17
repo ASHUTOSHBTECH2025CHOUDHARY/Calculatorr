f5b118cccf760dc458722dd01dc457a75b898232
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2025-01-17 14:48:36 +0530

    first commit

--- Substractionnumbers.java
+++ Substractionnumbers.java
@@ -1,4 +1,4 @@
-public class Substraction {
+public class Substractionnumbers {
         public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
             int a=sc.nextInt();
