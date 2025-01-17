853bd119ed85bda154019a71a1dc12131378c615
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2025-01-17 14:37:11 +0530

    first commit

--- Divisionnumbers.java
+++ Divisionnumbers.java
@@ -1,4 +1,6 @@
-public class Divsion {
+import java.util.Scanner;
+
+public class Divisionnumbers {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
